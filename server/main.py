ca800c5355f7c13849ec70cd1d8db0e17f56b21d
sertraline <36751007+sertraline@users.noreply.github.com>
2021-06-10 15:59:48 +0300

    ref: add cors middleware

--- server/main.py
+++ server/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from router import router
 import uvicorn
 
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host='0.0.0.0', port=8050)
